0e0fb511c6c96b749de7311ed9f3dcda84d1fb27
Exitare <business.exitare@icloud.com>
2018-11-12 17:31:45 -0800

    Add gitignore

--- start.py
+++ start.py
@@ -1,12 +1,14 @@
-class myObject:
+class plotValue:
     def __init__(self, id, number):
         self.id = id
         self.number = number
 
 
-time_traces = open("sampleData/time_traces.txt", "r").read().splitlines()
+
+
+time_traces = open("sampleData/time_traces.txt", "r").read()
 plot_values = open("sampleData/plot_values.txt", "r").read().splitlines()
-objectArray = []
+plotValueArray = []
 splitArray = []
 
 
@@ -18,12 +20,13 @@ for i in plot_values:
 
 
 for i in splitArray:
-    objectArray.append(myObject(i[0], i[1]))
+    plotValueArray.append(plotValue(i[0], i[1]))
 
 
 
-for object in objectArray:
-    print(object.id)
+print(time_traces)
+
+
 
 
 
