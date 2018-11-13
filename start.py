eaec06d5f7ac4f37d2b9cb69d18d5013e7712090
Exitare <business.exitare@icloud.com>
2018-11-13 11:35:14 -0800

    Add new Dir, add File Output

--- start.py
+++ start.py
@@ -1,33 +1,87 @@
-class plotValue:
+import re
+import numpy as np
+import os.path
+from enum import Enum
+from random import randint
+
+
+class Paths(Enum):
+    SAMPLE_DIR = 'sampleData/'
+    INPUT_DIR = 'Input/'
+    OUTPUT_DIR = 'Output/'
+    OUTPUT_FILE_NAME = 'Results.txt'
+    OUTPUT_PATH = 'Output/Results.txt'
+
+
+class PlotValue:
     def __init__(self, id, number):
         self.id = id
         self.number = number
 
 
+class Cell:
+
+    def __init__(self, id, mean):
+        self.id = id
+        self.mean = mean
 
 
-time_traces = open("sampleData/time_traces.txt", "r").read()
 plot_values = open("sampleData/plot_values.txt", "r").read().splitlines()
 plotValueArray = []
-splitArray = []
+splitPlotValueArray = []
 
-
-
-
-
+## Plot Values
 for i in plot_values:
-    splitArray.append((i.split('\t', 1)))
+    splitPlotValueArray.append((i.split('\t', 1)))
+
+for i in splitPlotValueArray:
+    plotValueArray.append(PlotValue(i[0], i[1]))
+
+## Plot Values End
 
 
-for i in splitArray:
-    plotValueArray.append(plotValue(i[0], i[1]))
+##Time Traces
 
 
+timeTracesFileData = open("sampleData/time_traces.txt", "r")
+rows = (row.strip().split() for row in timeTracesFileData)
+timeTraces = zip(*(row for row in rows if row))
+timeTracesFileData.close()
 
-print(time_traces)
+CellData = []
 
+rowCount = 0
+detectedColumns = 0
+counter = 0
+for timeTrace in timeTraces:
+    counter = 0
+    for timeData in timeTrace[1:]:
+        rowCount = float(timeData) + rowCount
+        counter += 1
 
+    else:
+        detectedColumns += 1
+        mean = np.mean(rowCount, dtype=np.float64)
+        CellData.append(Cell(timeTrace[0], mean))
 
+print('Detected Headers including Avg and Err Column: ', detectedColumns)
 
+print(Paths.OUTPUT_PATH.value)
 
-#print([i.split('\t', 1)[0] for i in plot_values])
+fileExists = os.path.exists(Paths.OUTPUT_PATH.value)
+
+print(fileExists)
+if fileExists:
+    os.remove(Paths.OUTPUT_PATH.value)
+
+fh = open(Paths.OUTPUT_PATH.value, "w")
+writtenColumns = 0
+
+for cell in CellData:
+    temp = 'ID: %s :  Mean: %d ' % (cell.id, cell.mean)
+    fh.write(temp)
+    writtenColumns += 1
+fh.close()
+
+print('Creating File %s' % Paths.OUTPUT_PATH.value)
+print('Written Columns: ', writtenColumns)
