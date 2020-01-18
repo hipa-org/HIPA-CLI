86a085ee1131e68ed5affcd67ccaa9ae4f1e805f
Exitare <business.exitare@icloud.com>
2020-01-18 12:30:12 -0800

    Latest updates

--- Services/Filemanagement/Write.py
+++ Services/Filemanagement/Write.py
@@ -7,34 +7,17 @@ import pandas as pd
 
 
 def write_high_intensity_counts(file: InputFile):
-    """
-    Write high intensity counts to a file
-    :return:
-    """
     now = datetime.datetime.now()
-    file_data = []
+    temp_list = list()
+
     for cell in file.cells:
-        temp_array = []
-        temp_array.append(cell.name)
+        temp_list.append(cell.high_intensity_counts)
 
-        for key, value in cell.high_intensity_counts.items():
-            temp_array.append(value)
+    df = pd.DataFrame(temp_list)
 
-        file_data.append(temp_array)
-
-    data = np.array(file_data)
-    data = data.T
-
-    try:
-        filename = '{0}_{1}{2}'.format(Config.Config.OUTPUT_FILE_NAME_HIGH_STIMULUS,
-                                       now.strftime("%Y-%m-%d %H-%M-%S"), '.txt')
-        np.savetxt(
-            '{0}/{1}'.format(file.folder, filename), data, fmt='%s', delimiter='\t')
-        Log.write_message(
-            'Created File {0} in {1}'.format(filename, file.folder), Log.LogLevel.Info)
-    except FileNotFoundError as ex:
-        Log.write_message('Error creating File!', Log.LogLevel.Error)
-        Log.write_message(ex, Log.LogLevel.Error)
+    df.to_csv(
+        file.folder + Config.Config.OUTPUT_FILE_NAME_NORMALIZED_DATA + "-" + now.strftime("%Y-%m-%d %H-%M-%S") + ".txt",
+        index=None, sep='\t', mode='a')
 
 
 def write_normalized_timeframes(file: InputFile):
