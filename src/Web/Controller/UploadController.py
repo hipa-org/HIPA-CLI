79bea9214ce72fd69772eba26cbb89c7eaa99368
Exitare <business.exitare@icloud.com>
2020-08-09 15:05:21 -0700

    Added working upload.

--- src/Web/Controller/UploadController.py
+++ src/Web/Controller/UploadController.py
@@ -1,6 +1,11 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from werkzeug.datastructures import FileStorage
+from flask import request
 
 
 class UploadController(Resource):
     def post(self):
+        print(request)
+        uploaded_files = request.files.getlist("files")
+        print(uploaded_files)
         pass
